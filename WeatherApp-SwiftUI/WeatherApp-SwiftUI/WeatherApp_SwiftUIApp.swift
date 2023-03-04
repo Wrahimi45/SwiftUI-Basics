81f9c1e45153635d6ec806b7e733512c4c391882
Wahidullah Rahimi <wahidullahrahimi@Wahidullahs-MacBook-Pro.local>
2023-03-04 12:54:41 -0800

    Initial Commit

---
+++ WeatherApp-SwiftUI/WeatherApp-SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Wahidullah Rahimi on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
